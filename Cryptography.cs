c397981564f39b43ac5ebcd558225dfa6abfe6d2
anonymous <anonymous>
2021-01-08 22:30:58 +0700

    intital

---
+++ Cryptography.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Crypto
+{
+    public interface Cryptography
+    {
+        string Encrypt(string plainText, string passPhrase);
+        string Decrypt(string cipherText, string passPhrase);
+    }
+}
